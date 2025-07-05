41884bc42955db08142321a66fad520faa240255
Bank Sirawat <sirawat.i@linecorp.com>
2025-07-05 17:00:16 +0700

    fix: rebalance group strategy

--- config_sarama.go
+++ config_sarama.go
@@ -33,7 +33,7 @@ func setDefaultSetting(c sarama.Config) sarama.Config {
 	c.Consumer.Return.Errors = true
 	c.Consumer.Retry.Backoff = 1 * time.Second
 	c.Consumer.Group.ResetInvalidOffsets = true
-	c.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.BalanceStrategyRoundRobin}
+	c.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.NewBalanceStrategyRoundRobin()}
 
 	// default metadata config
 	c.Metadata.Retry.Max = 10
